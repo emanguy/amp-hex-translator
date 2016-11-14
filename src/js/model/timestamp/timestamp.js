eb25bdff9190ec838ff3ef44c9028ea60886481f
Tim Maher <tmaher@umbc.edu>
2016-11-14 17:50:04 -0500

    Add timestamp parser

---
+++ src/js/model/timestamp/timestamp.js
@@ -0,0 +1,39 @@
+var AMPParse = AMPParse || {};
+
+AMPParse.buildByte = function (hexadecimal)
+{
+	// Defensive programming
+	if (typeof hexadecimal === "undefined" || typeof hexadecimal !== "string")
+	{
+		throw new ReferenceError("Provided parameter does not exist or is not a string");
+	}
+	if (hexadecimal.length < 2)
+	{
+		throw new RangeError("The provided hex is too short to contain a byte.");
+	}
+
+	// Snag the relevant bytes and build return value object
+	var byteChars = hexadecimal.substring(0, 2);
+	var rawValue = parseInt(byteChars, 16);
+	var returnValue = {
+		type: "Byte",
+		value: rawValue,
+		binaryValue: ""
+	};
+
+	// Make the binary representation and append prefix
+	for (var i = 0; i < 8; i++)
+	{
+		returnValue.binaryValue = ( rawValue & 1 ).toString() + returnValue.binaryValue;
+		rawValue = rawValue >>> 1;
+	}
+
+	returnValue.binaryValue = "0b" + returnValue.binaryValue;
+
+	// Return object according to spec
+	return {
+		returnValue: returnValue,
+		nibblesConsumed: 2,
+		trailingHex: hexadecimal.substring(2)
+	};
+}
