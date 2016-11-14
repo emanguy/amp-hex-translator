7c7a4a0f579ec9de56319e9a71a63153b5152c0c
Tim Maher <tmaher@umbc.edu>
2016-11-14 18:08:00 -0500

    still working on timestamps

--- src/js/model/timestamp/timestamp.js
+++ src/js/model/timestamp/timestamp.js
@@ -1,39 +1,49 @@
 var AMPParse = AMPParse || {};
 
-AMPParse.buildByte = function (hexadecimal)
+AMPParse.buildTimestamp = function (hexadecimal)
 {
 	// Defensive programming
-	if (typeof hexadecimal === "undefined" || typeof hexadecimal !== "string")
-	{
-		throw new ReferenceError("Provided parameter does not exist or is not a string");
-	}
-	if (hexadecimal.length < 2)
-	{
-		throw new RangeError("The provided hex is too short to contain a byte.");
-	}
+    if (typeof hexadecimal === "undefined" || typeof hexadecimal !== "string")
+    {
+	throw new ReferenceError("Provided parameter does not exist or is not a string");
+    }
 
-	// Snag the relevant bytes and build return value object
-	var byteChars = hexadecimal.substring(0, 2);
-	var rawValue = parseInt(byteChars, 16);
-	var returnValue = {
-		type: "Byte",
-		value: rawValue,
-		binaryValue: ""
-	};
+    // TODO: Call SDNV parser to get true value
+    sdnvReturn = atoi(hexadecimal);
+    
+    // Snag the relevant bytes and build return value object
+    var ampEpoch = new Data(0);
+    ampEpoch.setUTCSeconds(1347148800);
 
-	// Make the binary representation and append prefix
-	for (var i = 0; i < 8; i++)
-	{
-		returnValue.binaryValue = ( rawValue & 1 ).toString() + returnValue.binaryValue;
-		rawValue = rawValue >>> 1;
-	}
+    var returnData = new Data(0);
+    returnData.setUTCSeconds(sdnvReturn);
 
-	returnValue.binaryValue = "0b" + returnValue.binaryValue;
-
-	// Return object according to spec
-	return {
-		returnValue: returnValue,
-		nibblesConsumed: 2,
-		trailingHex: hexadecimal.substring(2)
-	};
+    relative = false;
+    if (returnData < ampEpoch) {
+	relavite = true;
+    }
+    else {
+	relative = false;
+    }
+    var returnValue = {
+	type: "Timestamp",
+	value: returnData,
+	isRelative: relative
+    };
+    
+    // Make the binary representation and append prefix
+    for (var i = 0; i < 8; i++)
+    {
+	returnValue.binaryValue = ( rawValue & 1 ).toString() + returnValue.binaryValue;
+	rawValue = rawValue >>> 1;
+    }
+    
+    returnValue.binaryValue = "0b" + returnValue.binaryValue;
+    
+    // Return object according to spec
+    return {
+	returnValue: returnValue,
+	nibblesConsumed: 2,
+	trailingHex: hexadecimal.substring(2)
+    };
 }
