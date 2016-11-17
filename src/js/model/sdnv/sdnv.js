e2610ab373247c4b043e96dfdc92f35db882ec10
Evan Rittenhouse <emanguy1@gmail.com>
2016-11-16 22:44:37 -0500

    Add check for SDNV never terminating.

--- src/js/model/sdnv/sdnv.js
+++ src/js/model/sdnv/sdnv.js
@@ -21,6 +21,12 @@ AMPParse.buildSdnv = function(hexadecimal)
 	// Keep adding onto the result as long as there's a 1 in the most significant bit
 	do
 	{
+		// Check to make sure the SDNV actually terminates
+		if (nextHex.length < 2)
+		{
+			throw new RangeError("The SDNV never terminated.");
+		}
+
 		// Get the value of the next byte and add it onto the hex representation
 		nextByte = parseInt( nextHex.substring(0,2), 16 );
 		hexRepresentation += nextHex.substring(0,2);
