37bdaa135f5c2adb0c6c68be09d5ec8990509b32
Nick <nicholasharman@resnet-231-158.resnet.umbc.edu>
2016-11-22 17:12:42 -0500

    Combine MID code into a single file.  Remove data types that are not AMP Structures.

--- src/js/model/mid/mid.js
+++ src/js/model/mid/mid.js
@@ -61,6 +61,81 @@ AMPParse.buildMid = function(hexadecimal) {
     }
 }
 
+AMPParse.buildMidFlag = function(hexadecimal) {
+
+    if (!(hexadecimal instanceof AMPHexConsumer)) {
+        throw new ReferenceError("Did not receive an AMPHexConsumer");
+    }
+
+    hexadecimal.consumeNibbles();
+    var byteValue = hexadecimal.consumedHexInt;
+
+    var OID = (byteValue & 192) >> 6;
+    var hasTag = ((byteValue & 32) >> 5) === 1;
+    var hasIssuer = ((byteValue & 16) >> 4) === 1;
+
+    // TODO: verify this is right, sturctures go up to 25 so why only 4 bits?
+    var structureId = byteValue & 15;
+
+    var isCompressed, isParametrized = false;
+
+    switch(OID) {
+        case 0:
+            isParametrized = false;
+            isCompressed = false;
+            break;
+        case 1:
+            isParametrized = true;
+            isCompressed = false;
+            break;
+        case 2:
+            isParametrized = false;
+            isCompressed = true;
+            break;
+        case 3:
+            isParametrized = true;
+            isCompressed = true;
+            break;
+        default:
+            throw new ReferenceError("Invalid OID was present in the MID Flag");
+    }
+
+    var returnValue = {
+        isCompressed: isCompressed,
+        isParametrized: isParametrized,
+        hasTag: hasTag,
+        hasIssuer: hasIssuer,
+        enclosedStructId: AMPParse.getStructTypeFromId(structureId).name
+    }
+
+    return {
+        returnValue: returnValue,
+        nibblesConsumed: 2
+    }
+
+}
+
+AMPParse.getStructTypeFromId = function(structureId) {
+    if (isNaN(structureId) || structureId < 0 || structureId > 8) {
+        throw new ReferenceError("Invalid StructureId: " + structureId +
+            " StructureId must be a number between 0 and 25");
+    }
+
+    var structureTypes = [
+        {name: "Externally Defined Data", id: "EDD", enumeration: 0},
+        {name: "Variable", id: "VAR", enumeration: 1},
+        {name: "Report", id: "RPT", enumeration: 2},
+        {name: "Control", id: "CTRL", enumeration: 3},
+        {name: "State-Based Rule", id: "SRL", enumeration: 4},
+        {name: "Time-Based Rule ", id: "TRL", enumeration: 5},
+        {name: "Macro", id: "MACRO", enumeration: 6},
+        {name: "Literal", id: "LIT", enumeration: 7},
+        {name: "Operator", id: "OP", enumeration: 8}
+    ]
+
+    return structureTypes[structureId];
+}
+
 // TODO: remove this stub method when the real implementation is ready
 AMPParse.buildObjectIdentifier = function(hexadecimal) {
     return {returnValue: {type: "Object Identifier"}};
