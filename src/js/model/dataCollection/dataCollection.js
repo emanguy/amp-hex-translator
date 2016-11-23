99a0a496830fff67a6550c216adf5644e81c7755
Evan Rittenhouse <emanguy1@gmail.com>
2016-11-23 03:51:32 -0500

    Initial Data Collection implementation w/o tests

--- src/js/model/dataCollection/dataCollection.js
+++ src/js/model/dataCollection/dataCollection.js
@@ -1,39 +1,148 @@
 var AMPParse = AMPParse || {};
 
-AMPParse.buildByte = function (hexadecimal)
+AMPParse.buildDataCollection = function (hexadecimal, isTyped)
 {
-	// Defensive programming
-	if (typeof hexadecimal !== "string")
+	// Defensive Programming
+	if ( !(hexadecimal instanceof AMPHexConsumer) )
 	{
-		throw new ReferenceError("Provided parameter does not exist or is not a string");
+		throw new TypeError("Did not receive an instance of AMPHexConsumer");
 	}
-	if (hexadecimal.length < 2 || hexadecimal.length % 2 !== 0)
+	if (typeof isTyped !== "boolean")
 	{
-		throw new RangeError("The provided hex is too short to contain a byte or is misaligned.");
-	}
-
-	// Snag the relevant bytes and build return value object
-	var byteChars = hexadecimal.substring(0, 2);
-	var rawValue = parseInt(byteChars, 16);
-	var returnValue = {
-		type: "Byte",
-		value: rawValue,
-		binaryValue: ""
-	};
-
-	// Make the binary representation and append prefix
-	for (var i = 0; i < 8; i++)
-	{
-		returnValue.binaryValue = ( rawValue & 1 ).toString() + returnValue.binaryValue;
-		rawValue = rawValue >>> 1;
+		throw new TypeError("isTyped is not a boolean value.");
 	}
 
-	returnValue.binaryValue = "0b" + returnValue.binaryValue;
+	var numBlobs;
+	var nibblesConsumed;
 
-	// Return object according to spec
+	// Get the number of "blobs" contained
+	try
+	{
+		numBlobs = AMPParse.buildSdnv(hexadecimal);
+		nibblesConsumed = numBlobs.nibblesConsumed;
+		numBlobs = numBlobs.returnValue.value;
+	}
+	catch (err)
+	{
+		throw err;
+	}
+
+	var types;
+
+	// Build the array of types to read
+	if (isTyped)
+	{
+		// For typed, we just read in the type attributes
+		try
+		{
+			types = AMPParse.buildBlob(hexadecimal);
+		}
+		catch (err)
+		{
+			err.nibblesConsumed += nibblesConsumed;
+			throw err;
+		}
+
+		nibblesConsumed += types.nibblesConsumed;
+		types = types.returnValue.value.map(function(arrItem) {
+			return arrItem.value;
+		});
+
+		numBlobs--;
+	}
+	else
+	{
+		// For non typed we just make an array of byte readers
+		types = new Array(numBlobs);
+
+		for (var index in types)
+		{
+			types[index] = 9;
+		}
+	}
+
+	// Build the array of contained values
+	var elementArray = [];
+	var element;
+	var parameters;
+	var fnCall;
+	var type;
+
+	while (!hexadecimal.isEmpty && types.length > 0)
+	{
+		try
+		{
+			type = types.shift();
+			parameters = AMPBuilders[type].slice(); // Creates a copy of the array
+		}
+		catch (err)
+		{
+			err.nibblesConsumed += nibblesConsumed;
+			err.message = "Unknown type: " + type;
+			throw err;
+		}
+
+		// Move the method name into fnCall and replace it with hexadecimal consumer
+		fnCall = parameters.splice(0, 1, hexadecimal)[0];
+		
+		try
+		{
+			// Here I use numBlobs to specify how many bytes I expect to read
+			numBlobs = AMPParse.buildSdnv(hexadecimal);
+			nibblesConsumed += numBlobs.nibblesConsumed;
+			numBlobs = numBlobs.returnValue.value;
+
+			element = AMPParse[fnCall].apply(AMPParse[fnCall], parameters);
+			
+			// Check to see that the correct number of bytes was consumed
+			if (numBlobs !== element.nibblesConsumed / 2)
+			{
+				throw new RangeError("SDNV lied about blob length");
+			}
+
+			nibblesConsumed += element.nibblesConsumed;
+			elementArray.push(element.returnValue);
+		}
+		catch (err)
+		{
+			err.nibblesConsumed += nibblesConsumed;
+			throw err;
+		}
+
+	}
+
+	if (hexadecimal.isEmpty && types.length > 0)
+	{
+		throw new RangeError("Data Collection specified more bytes than were available.");
+	}
+
 	return {
-		returnValue: returnValue,
-		nibblesConsumed: 2,
-		trailingHex: hexadecimal.substring(2)
+		returnValue: {
+			type: "Data Collection",
+			value: elementArray,
+			isTyped: isTyped
+		},
+		nibblesConsumed: nibblesConsumed
 	};
+}
+
+// This is used so that inclusion order doesn't matter other than buildSdnv and buildBlob
+AMPBuilders = {
+	9: [ "buildByte" ],
+	10: [ "buildBasicNumber", "Integer", false ],
+	11: [ "buildBasicNumber", "Integer", true ],
+	12: [ "buildBasicNumber", "Vast", false ],
+	13: [ "buildBasicNumber", "Vast", true ],
+	14: [ "buildFloatNumber", "32" ],
+	15: [ "buildFloatNumber", "64" ],
+	16: [ "buildSdnv" ],
+	17: [ "buildTimestamp" ],
+	18: [ "buildString" ],
+	19: [ "buildBlob" ],
+	20: [ "buildManagedIdentifier" ],
+	21: [ "buildMIDCollection" ],
+	22: [ "buildExpression" ],
+	23: [ "buildDataCollection", false ],
+	24: [ "buildDataCollection", true ],
+	25: [ "buildTable" ]
 }
