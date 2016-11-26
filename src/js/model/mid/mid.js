e68b4c9838c5123bfebf885f2591247647ce328a
Nick <nicholasharman@Nicholass-MacBook-Pro-2.local>
2016-11-26 14:01:19 -0500

    Clean up code per code review.

--- src/js/model/mid/mid.js
+++ src/js/model/mid/mid.js
@@ -78,35 +78,15 @@ AMPParse.buildMidFlag = function(hexadecimal) {
 
     var structureId = byteValue & 15;
 
-    var isCompressed, isParametrized = false;
-
-    switch(OID) {
-        case 0:
-            isParametrized = false;
-            isCompressed = false;
-            break;
-        case 1:
-            isParametrized = true;
-            isCompressed = false;
-            break;
-        case 2:
-            isParametrized = false;
-            isCompressed = true;
-            break;
-        case 3:
-            isParametrized = true;
-            isCompressed = true;
-            break;
-        default:
-            throw new ReferenceError("Invalid OID was present in the MID Flag");
-    }
+    var isParametrized = (OID & 1) === 1;
+    var isCompressed = (OID & 2) === 2;
 
     var returnValue = {
         isCompressed: isCompressed,
         isParametrized: isParametrized,
         hasTag: hasTag,
         hasIssuer: hasIssuer,
-        enclosedStructId: AMPParse.getStructTypeFromId(structureId).name
+        enclosedStructId: AMPParse.getStructTypeFromId(structureId)
     }
 
     return {
@@ -124,15 +104,15 @@ AMPParse.getStructTypeFromId = function(structureId) {
     }
 
     var structureTypes = [
-        {name: "Externally Defined Data", id: "EDD", enumeration: 0},
-        {name: "Variable", id: "VAR", enumeration: 1},
-        {name: "Report", id: "RPT", enumeration: 2},
-        {name: "Control", id: "CTRL", enumeration: 3},
-        {name: "State-Based Rule", id: "SRL", enumeration: 4},
-        {name: "Time-Based Rule ", id: "TRL", enumeration: 5},
-        {name: "Macro", id: "MACRO", enumeration: 6},
-        {name: "Literal", id: "LIT", enumeration: 7},
-        {name: "Operator", id: "OP", enumeration: 8}
+        "Externally Defined Data",
+        "Variable",
+        "Report",
+        "Control",
+        "State-Based Rule",
+        "Time-Based Rule ",
+        "Macro",
+        "Literal",
+        "Operator"
     ]
 
     return structureTypes[structureId];
